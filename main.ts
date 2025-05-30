255816bd5803eaaf3ebb6ba50e8ce81be586ffe4
wwib420 <grst3787@cloud.edu.pe.ca>
2025-05-30 14:59:51 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+let menu_2 = 0
+let radio_group = 0
+input.onButtonPressed(Button.A, function () {
+    if (menu_2 == 2) {
+    	
+    } else {
+        radio_group += -1
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (menu_2 == 2) {
+        menu_2 = 1
+    } else {
+        radio_group += 1
+    }
+})
+basic.forever(function () {
+    radio.setGroup(0 + radio_group)
+    if (menu_2 == 2) {
+        basic.showLeds(`
+            . # # # .
+            . # . # .
+            . # # # .
+            . # # . .
+            . # . # .
+            `)
+    } else {
+        basic.showString("" + (radio_group))
+    }
+})
